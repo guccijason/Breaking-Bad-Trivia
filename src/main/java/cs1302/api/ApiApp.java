9554d53cb3e8909220893e897023e5b3d79976e0
Jason Le <jql58922@uga.edu>
2023-05-05 14:49:52 -0400

    one api done

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -14,12 +14,39 @@ import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 /**
  * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
  */
 public class ApiApp extends Application {
+
+    /** HTTP client. */
+    public static HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)           // uses HTTP protocol version 2 where possible
+        .followRedirects(HttpClient.Redirect.NORMAL)  // always redirects, except from HTTPS to HTTP
+        .build();                                     // builds and returns an HttpClient
+
+    public static Gson GSON = new GsonBuilder()
+        .setPrettyPrinting()
+        .create();
+
+    private static final String BREAKING_API = "https://api.breakingbadquotes.xyz/v1/quotes";
+
     Stage stage;
     Scene scene;
     VBox root;
@@ -28,15 +55,19 @@ public class ApiApp extends Application {
     HBox row1, row2, row3;
     Label title;
     Label question;
+    Label quote;
     Button choice1, choice2, choice3;
     ImageView imgv1, imgv2, imgv3;
     ImageView titlePic;
+    Button start;
+    HBox thirdLay;
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
      * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
      */
     public ApiApp() {
+        start = new Button("Start");
         root = new VBox();
         stage = null;
         scene = null;
@@ -45,6 +76,7 @@ public class ApiApp extends Application {
         row3 = new HBox();
         topLay = new HBox();
         secLay = new HBox();
+        thirdLay = new HBox();
         imgv1 = new ImageView();
         imgv2 = new ImageView();
         imgv3 = new ImageView();
@@ -53,6 +85,7 @@ public class ApiApp extends Application {
         choice3 = new Button();
         titlePic = new ImageView();
         question = new Label("Who said this quote?");
+        quote = new Label("QUOTE");
         title = new Label("BREAKING BAD TRIVIA");
     } // ApiApp
 
@@ -60,15 +93,21 @@ public class ApiApp extends Application {
     @Override
     public void init() {
         //root.setPrefWidth(625);
-        root.getChildren().addAll(topLay, titlePic, secLay, row1, row2, row3);
+        root.getChildren().addAll(topLay, titlePic, secLay, thirdLay, row1, row2, row3);
         topLay.getChildren().add(title);
+        topLay.setAlignment(Pos.CENTER);
+
 
         String breakBadURL = "resources/breakingbad-large-logo.png";
         Image breakBadImg = new Image("file:" + breakBadURL, 500, 125, false, false);
         titlePic.setImage(breakBadImg);
 
         topLay.setHgrow(title, Priority.ALWAYS);
-        secLay.getChildren().add(question);
+        secLay.getChildren().addAll(question, start);
+        secLay.setAlignment(Pos.CENTER);
+        thirdLay.getChildren().add(quote);
+        thirdLay.setAlignment(Pos.CENTER);
+
         secLay.setHgrow(question, Priority.ALWAYS);
         row1.getChildren().addAll(choice1, imgv1);
         row2.getChildren().addAll(choice2, imgv2);
@@ -87,6 +126,9 @@ public class ApiApp extends Application {
         choice3.setMaxHeight(Double.MAX_VALUE);
 
         setDefault();
+        quotesSetUp();
+
+
     }
 
     /** {@inheritDoc} */
@@ -95,17 +137,6 @@ public class ApiApp extends Application {
 
         this.stage = stage;
 
-        // demonstrate how to load local asset using "file:resources/"
-        /*Image bannerImage = new Image("file:resources/readme-banner.png");
-        ImageView banner = new ImageView(bannerImage);
-        banner.setPreserveRatio(true);
-        banner.setFitWidth(640);
-
-        // some labels to display information
-        Label notice = new Label("Modify the starter code to suit your needs.");
-
-        // setup scene
-        root.getChildren().addAll(banner, notice);*/
         scene = new Scene(root);
 
         // setup stage
@@ -126,6 +157,43 @@ public class ApiApp extends Application {
         imgv1.setImage(defaultImg);
         imgv2.setImage(defaultImg);
         imgv3.setImage(defaultImg);
+    }
+
+    /**
+     * Sets up the quotes.
+     */
+    private void quotesSetUp() {
+        try {
+            String uri = BREAKING_API;
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .build();
+            // send request / receive response in the form of a String
+            HttpResponse<String> response = HTTP_CLIENT
+                .send(request, BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new IOException(response.toString());
+            } // if
+            String jsonString = response.body();
+            System.out.println("********** RAW JSON STRING: **********");
+            System.out.println(jsonString.trim());
+            // parse the JSON-formatted string using GSON
+            BreakingQuotes[] breakingquotes = GSON
+                .fromJson(jsonString, BreakingQuotes[].class);
+            getQuote(breakingquotes);
+        } catch (IOException | InterruptedException e) {
+
+            System.err.println(e);
+            e.printStackTrace();
+        } // try
+    }
+
+    /**
+     * Retrieves the quote from the api.
+     * @param breakingquotes the response object
+     */
+    private void getQuote(BreakingQuotes[] breakingquotes) {
+        this.quote.setText(breakingquotes[0].quote);
     }
 
 } // ApiApp
---
+++ src/main/java/cs1302/api/BreakingQuotes.java
@@ -0,0 +1,12 @@
+package cs1302.api;
+
+/**
+ * Represents the quote from the Breaking Bad Quotes API.
+ *
+ */
+public class BreakingQuotes {
+
+    String quote;
+    String author;
+
+} // BreakingQuotes
