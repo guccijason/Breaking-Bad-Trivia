65a2bdc72beef9db02404bc7634453593cf21a10
Jason Le <jql58922@uga.edu>
2023-05-07 01:47:26 -0400

    got summary

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -30,10 +30,13 @@ import java.nio.charset.StandardCharsets;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
-
+import java.util.Random;
 
 /**
- * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
+ * Breaking Bad trivia game that gets quotes from the show.
+ * Uses the Breaking Bad API and TV Maze API
+ * When it is a correct answer the app gives information about
+ * the actor that said the quote.
  */
 public class ApiApp extends Application {
 
@@ -48,10 +51,11 @@ public class ApiApp extends Application {
         .create();
 
     private static final String BREAKING_API = "https://api.breakingbadquotes.xyz/v1/quotes";
-    private static final String BREAKING_INFO = "https://api.tvmaze.com/shows/169/";
+    private static final String BREAKING_INFO = "https://api.tvmaze.com/shows/169";
 
     Stage stage;
     Scene scene;
+    HBox tree;
     VBox root;
     HBox topLay;
     HBox secLay;
@@ -66,20 +70,25 @@ public class ApiApp extends Application {
     HBox thirdLay;
     ArrayList<String> charNames;
     ArrayList<String> charImageUrls;
+    String[] buttons;
     Boolean correctAnswer;
     String quoteName;
+    Label infoBox;
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
      * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
      */
     public ApiApp() {
+        infoBox = new Label("Breaking Bad is a tv show");
+        buttons = new String[3];
         quoteName = "";
         correctAnswer = false;
         charNames = new ArrayList<String>();
         charImageUrls = new ArrayList<String>();
         start = new Button("Start");
         root = new VBox();
+        tree = new HBox();
         stage = null;
         scene = null;
         row1 = new HBox();
@@ -103,7 +112,10 @@ public class ApiApp extends Application {
     /** {@inheritDoc} */
     @Override
     public void init() {
-        //root.setPrefWidth(625);
+        tree.setMaxWidth(750);
+        infoBox.setWrapText(true);
+        tree.getChildren().addAll(root, infoBox);
+        root.setMaxWidth(500);
         root.getChildren().addAll(topLay, titlePic, secLay, thirdLay, row1, row2, row3);
         topLay.getChildren().add(title);
         topLay.setAlignment(Pos.CENTER);
@@ -132,6 +144,7 @@ public class ApiApp extends Application {
         choice3.setMaxHeight(Double.MAX_VALUE);
 
         setDefault();
+        tvSumSetUp();
 
         EventHandler<ActionEvent> startGame = (ActionEvent e) -> {
             quotesSetUp();
@@ -139,6 +152,8 @@ public class ApiApp extends Application {
             start.setText("Reset");
         };
         start.setOnAction(startGame);
+
+
     }
 
     /** {@inheritDoc} */
@@ -147,7 +162,7 @@ public class ApiApp extends Application {
 
         this.stage = stage;
 
-        scene = new Scene(root);
+        scene = new Scene(tree);
 
         // setup stage
         stage.setTitle("ApiApp!");
@@ -217,7 +232,7 @@ public class ApiApp extends Application {
     private void tvInfoSetUp() {
         try {
             // form URI
-            String uri = BREAKING_INFO + "cast";
+            String uri = BREAKING_INFO + "/cast";
             // build request
             HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
@@ -256,25 +271,7 @@ public class ApiApp extends Application {
             charNames.add(tvinfo[i].character.name);
             charImageUrls.add(tvinfo[i].character.image.medium);
         }
-        for (int i = 0; i < charNames.size(); i++) {
-            System.out.println(charNames.get(i));
-            System.out.println(charImageUrls.get(i));
-        }
-        int randNum = (int) (Math.random() * charNames.size());
-        int randNum1 = (int) (Math.random() * charNames.size());
-        int randNum2 = (int) (Math.random() * charNames.size());
-
-        checkForDups(randNum, randNum1, randNum2);
-
-        Image temp1 = new Image(charImageUrls.get(randNum), 175, 175, false, false);
-        Image temp2 = new Image(charImageUrls.get(randNum1), 175, 175, false, false);
-        Image temp3 = new Image(charImageUrls.get(randNum2), 175, 175, false, false);
-        this.imgv1.setImage(temp1);
-        this.imgv2.setImage(temp2);
-        this.imgv3.setImage(temp3);
-        this.choice1.setText(charNames.get(randNum));
-        this.choice2.setText(charNames.get(randNum1));
-        this.choice3.setText(charNames.get(randNum2));
+        checkForDups();
         getNameFromQuote();
     } // addImages
 
@@ -283,37 +280,41 @@ public class ApiApp extends Application {
      */
     private void getNameFromQuote() {
         int index = 0;
+        boolean isThere = (buttons[0].equals(quoteName) || buttons[1].equals(quoteName)
+            || buttons[2].equals(quoteName));
         int randNum = (int) (Math.random() * 3) + 1;
-        if (charNames.contains(quoteName)) {
+        if (!(isThere)) {
+            if (charNames.contains(quoteName)) {
 
-            index = charNames.indexOf(quoteName);
-            setRandom(randNum, index);
+                index = charNames.indexOf(quoteName);
+                setRandom(randNum, index);
 
 
-        } else if (quoteName.equals("Gustavo Fring")) {
+            } else if (quoteName.equals("Gustavo Fring")) {
 
-            index = charNames.indexOf("Gustavo 'Gus' Fring");
-            setRandom(randNum, index);
+                index = charNames.indexOf("Gustavo 'Gus' Fring");
+                setRandom(randNum, index);
 
-        } else if (quoteName.equals("Mike Ehrmantraut")) {
+            } else if (quoteName.equals("Mike Ehrmantraut")) {
 
-            index = charNames.indexOf("Michael 'Mike' Ehrmantraut");
-            setRandom(randNum, index);
+                index = charNames.indexOf("Michael 'Mike' Ehrmantraut");
+                setRandom(randNum, index);
 
-        } else {
-            String defaultURL = "resources/walter-whiteFace.png";
-            Image defaultImg = new Image("file:" + defaultURL, 175, 175, false, false);
-            if (randNum == 1) {
-                this.choice1.setText("None of these");
-                this.imgv1.setImage(defaultImg);
-            }
-            if (randNum == 2) {
-                this.choice2.setText("None of these");
-                this.imgv2.setImage(defaultImg);
-            }
-            if (randNum == 3) {
-                this.choice3.setText("None of these");
-                this.imgv3.setImage(defaultImg);
+            } else {
+                String defaultURL = "resources/walter-whiteFace.png";
+                Image defaultImg = new Image("file:" + defaultURL, 175, 175, false, false);
+                if (randNum == 1) {
+                    this.choice1.setText("None of these");
+                    this.imgv1.setImage(defaultImg);
+                }
+                if (randNum == 2) {
+                    this.choice2.setText("None of these");
+                    this.imgv2.setImage(defaultImg);
+                }
+                if (randNum == 3) {
+                    this.choice3.setText("None of these");
+                    this.imgv3.setImage(defaultImg);
+                }
             }
         }
     } // getNameFromQuote
@@ -341,17 +342,85 @@ public class ApiApp extends Application {
 
     /**
      * Helper method to check for duplicate random numbers.
-     * @param randNum1
-     * @param randNum2
-     * @param randNum3
      */
-    private void checkForDups(int randNum1, int randNum2, int randNum3) {
-        while (randNum1 == randNum2 || randNum1 == randNum3 || randNum2 == randNum3) {
-            randNum1 = (int) (Math.random() * charNames.size());
-            randNum2 = (int) (Math.random() * charNames.size());
-            randNum3 = (int) (Math.random() * charNames.size());
+    private void checkForDups() {
+
+        Random rand = new Random();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < charNames.size(); i++) {
+            list.add(i);
         }
+        int randNum1 = rand.nextInt(list.size());
+        list.remove(randNum1);
+        int randNum2 = rand.nextInt(list.size());
+        list.remove(randNum2);
+        int randNum3 = rand.nextInt(list.size());
+        list.remove(randNum3);
+        Image temp1 = new Image(charImageUrls.get(randNum1), 175, 175, false, false);
+        Image temp2 = new Image(charImageUrls.get(randNum2), 175, 175, false, false);
+        Image temp3 = new Image(charImageUrls.get(randNum3), 175, 175, false, false);
+        this.imgv1.setImage(temp1);
+        this.imgv2.setImage(temp2);
+        this.imgv3.setImage(temp3);
+        this.choice1.setText(charNames.get(randNum1));
+        this.choice2.setText(charNames.get(randNum2));
+        this.choice3.setText(charNames.get(randNum3));
+        buttons[0] = charNames.get(randNum1);
+        buttons[1] = charNames.get(randNum2);
+        buttons[2] = charNames.get(randNum3);
+        list.clear();
     }
 
+    /**
+     * Sets up the Breaking Bad tv show info.
+     */
+    private void tvSumSetUp() {
+        try {
+            // form URI
+            String uri = BREAKING_INFO;
+            // build request
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .build();
+            // send request / receive response in the form of a String
+            HttpResponse<String> response = HTTP_CLIENT
+                .send(request, BodyHandlers.ofString());
+            // ensure the request is okay
+            if (response.statusCode() != 200) {
+                throw new IOException(response.toString());
+            } // if
+            // get request body (the content we requested)
+            String jsonString = response.body();
+            //System.out.println("********** RAW JSON STRING: **********");
+            //System.out.println(jsonString.trim());
+            // parse the JSON-formatted string using GSON
+            BreakingBad breakingbad = GSON
+                .fromJson(jsonString, BreakingBad.class);
+            // print info about the response
+            addSummary(breakingbad);
+        } catch (IOException | InterruptedException e) {
+
+            System.err.println(e);
+            e.printStackTrace();
+        } // try
+    } //tvInfoSetUp
+
+    /**
+     * Helper method to add summary to infoBox.
+     * @param breakingbad
+     */
+    private void addSummary(BreakingBad breakingbad) {
+        infoBox.setText("SUMMARY OF THE SHOW\n" + breakingbad.summary);
+    }
+
+    /**
+     * Method that checks if the user's choice is correct.
+     * @param choice
+     */
+    private void checkIfCorrect(Button choice) {
+        if (choice.getText().equals(quoteName)) {
+            correctAnswer = true;
+        }
+    } // checkIfCorrect
 
 } // ApiApp
---
+++ src/main/java/cs1302/api/BreakingBad.java
@@ -0,0 +1,8 @@
+package cs1302.api;
+
+/**
+ * Collects summary from tv API.
+ */
+public class BreakingBad {
+    String summary;
+} // BreakingBad
